96c2dc90a3649fcd432f93c42ed51abd42c11305
Jon Olson <jon@damogran.com>
2009-11-18 20:35:41 -0500

    Changed organization name globally to Ballistic Pigeon, LLC

--- Testing/NSArrayExtensionsTest.h
+++ Testing/NSArrayExtensionsTest.h
@@ -3,7 +3,7 @@
 //  BPFoundationExtensions
 //
 //  Created by Jon Olson on 11/18/09.
-//  Copyright 2009 Jon Olson. All rights reserved.
+//  Copyright 2009 Ballistic Pigeon, LLC. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
